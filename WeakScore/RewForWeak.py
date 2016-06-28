a71c7ccb5aa0aafe46c6603aeae879cf7ecd4f1b
dave90 <davide@davide-All-Series>
2016-06-28 19:30:36 +0200

    Fix for first line

--- WeakScore/RewForWeak.py
+++ WeakScore/RewForWeak.py
@@ -1,29 +1,39 @@
 import fileinput
 
+def rewWeak(splitLine):
+        splitInstance=splitLine[0].split("/")
+        inst=""
+        for s in splitInstance[6:]:
+                inst = inst + s +"/"
+        inst =  inst[:-1]
+        print(inst+"\t"+splitLine[1]+"\t"+splitLine[2])
+
+
 i=0
+firstline=""
 for line in fileinput.input():
     if not i:
         i=1
+        firstline=line.rstrip("\n")
         continue
     l = line.rstrip("\n")
     splitLine = l.split("\t")
     if len(splitLine) == 3:
-    	splitInstance=splitLine[0].split("/")
-    	inst=""
-    	for s in splitInstance[6:]:
-     		inst = inst + s +"/"
-    	inst =  inst[:-1]
-    	print(inst+"\t"+splitLine[1]+"\t"+splitLine[2])
+        if firstline :
+            splitLineFirst = firstline.split("\t")
+            rewWeak(splitLineFirst)
+            firstline=""
+        rewWeak(splitLine)
 
     if len(splitLine) < 8:
         continue
-    
+
     if splitLine[7]=="yes":
-    	result="OK"
+        result="OK"
     elif splitLine[7]=="no":
-    	result="NO"
+        result="NO"
     else:
-    	continue
+        continue
     instance=splitLine[1].strip("('")
     instance2=instance.strip("',)")
     splitInstance=instance2.split("/")
@@ -32,6 +42,4 @@ for line in fileinput.input():
      inst = inst + s +"/"
     inst =  inst[:-1]
     print(inst+"\t"+splitLine[2]+"\t"+result)
-
-
 
